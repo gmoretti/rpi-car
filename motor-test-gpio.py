d4a8775a8cbd988e4545e9646a6d55d45235d608
gmoretti <moretti.giuseppe@gmail.com>
2021-01-04 22:30:08 +0100

    new test

---
+++ motor-test-gpio.py
@@ -0,0 +1,17 @@
+from gpiozero import Motor
+import time
+
+motor1 = Motor(27, 17)
+motor2 = Motor(24, 23)
+
+print('going forward')
+motor1.forward()
+motor2.forward()
+
+time.sleep(2)
+
+print('going backward')
+motor1.backward()
+motor2.backward()
+
+	
